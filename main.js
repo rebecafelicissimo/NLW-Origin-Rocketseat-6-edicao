d27d1385a10efe796decf47d02e08a72d0d576c3
appy-hour <rebeca.felicissimo@gmail.com>
2022-04-22 12:55:03 -0300

    create services and about
    criamos os cards e a rolagem suave na tag html

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //  quando eu clicar no ícone do menu, irá abrir o meu menu.
+// abre e fecha o menu quando clicar no ícone: hamburguer e x
 const nav = document.querySelector('#header nav')
 const navigationToggle = document.querySelectorAll('nav .toggle')
 
@@ -7,3 +8,14 @@ for (const element of navigationToggle) {
     nav.classList.toggle('show')
   })
 } // o meu elemento nesse caso é o ícone do hamburguer e meu x
+
+/* quando clicar em um item do menu, esconder o menu */
+const linksDoMenu = document.querySelectorAll('nav ul li a')
+
+for (const link of linksDoMenu) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* ====================================== */
