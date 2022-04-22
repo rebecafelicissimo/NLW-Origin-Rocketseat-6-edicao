15b5381a08c726d10057de1cb50913c3489d7405
appy-hour <rebeca.felicissimo@gmail.com>
2022-04-22 17:48:49 -0300

    create Scroll Reveal
    mostrar elementos quando der scroll na página

--- main.js
+++ main.js
@@ -42,3 +42,21 @@ const swiper = new Swiper('.swiper', {
   mousewheel: true,
   keyboard: true
 })
+
+/* Scroll Reveal: mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
